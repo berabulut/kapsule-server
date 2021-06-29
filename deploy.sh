dea4b06c1aeb1682bf2e6c4ffe6336518c0be8ef
Hüseyin Bera Bulut <berabulut@gmail.com>
2021-06-29 15:58:10 +0300

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -21,15 +21,18 @@ aws --region ${AWS_DEFAULT_REGION} ecr get-login-password \
 # pull latest images
 echo "Pulling images!"
 docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/kapsule/kapsule-ui:latest
-docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/kapsule/kapsule-service:latest
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/kapsule/kapsule-redirect:latest
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/kapsule/kapsule-api:latest
 
 # rename them
 docker image tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/kapsule/kapsule-ui:latest kapsule-ui:latest
-docker image tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/kapsule/kapsule-service:latest kapsule-service:latest
+docker image tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/kapsule/kapsule-redirect:latest kapsule-redirect:latest
+docker image tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/kapsule/kapsule-api:latest kapsule-api:latest
 
 # remove long named image
 docker rmi $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/kapsule/kapsule-ui
-docker rmi $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/kapsule/kapsule-service
+docker rmi $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/kapsule/kapsule-redirect
+docker rmi $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/kapsule/kapsule-api
 
 docker-compose down  
 docker-compose build  
